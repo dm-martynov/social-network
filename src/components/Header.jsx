2b4d318ddd60111056842584a62da81e4c4b911b
dmitrii_martynov <dmitriimartynov1@gmail.com>
2020-10-18 17:57:15 +0300

    started new project - social network. Added initial tags and components

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => (
+  <header className="header">
+    <img
+      alt="logo"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQmlvqP7RnoWAQGa5eDNf43h_CAH5FakZNzvg&usqp=CAU"
+    />
+  </header>
+);
+
+export default Header;
